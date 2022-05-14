f8ad6fa4e54650e20e4b2bed3fe7c2bbd5ba658f
Amirshokh Mukhitdinov <amirshoxmuxitdinov@gmail.com>
2022-05-14 16:15:56 +0500

    tests for endpoints

---
+++ test_routes.py
@@ -0,0 +1,20 @@
+from fastapi.testclient import TestClient
+
+from auth import AuthHandler
+from main import app
+
+client = TestClient(app)
+
+
+def test_get_users_without_token():
+    response = client.get("/users/")
+    assert response.status_code == 403
+    assert response.json() == {"detail": "Not authenticated"}
+
+
+def test_get_users_with_token():
+    auth = AuthHandler()
+    token = auth.encode_token(user_id=11)
+    headers = {"Authorization": f"Bearer {token}"}
+    response = client.get("/users/", headers=headers)
+    assert response.status_code == 200
